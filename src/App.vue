6355379b0de5be73bf4d60765e6a51b461fcc390
Zurc <cruzjurado@gmail.com>
2017-07-17 07:52:17 +0100

    bulma modal component added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
+
+    <modal v-if="showModal" @close="showModal = false"></modal>
     <message title="Hello World" body="Lorem ipsum dolor sit amet"></message>
-    
     <message title="Hello Universe" body="bla bla bla"></message>
+
+    <button class="button is-primary is-outlined" @click="showModal = true">Show modal</Button>
   </div>
 </template>
 
 <script>
   import message from '@/components/Message'
+  import modal from '@/components/Modal'
 
   export default {
     name: 'app',
     components: {
-      message
+      message, modal
+    },
+    data () {
+      return {
+        showModal: false
+      }
     }
   }
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <!-- Any other Bulma elements you want -->
+            <div class="box">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique fugiat magnam maiores quia rerum illum quidem quod temporibus eaque nam veritatis eius eos officia, nobis repellendus aspernatur. Quia, esse, qui!
+                </p>
+            </div>
+        </div>
+        <button class="modal-close is-large" @click="$emit('close')"></button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'modal',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+new Vue({
+  el: "#app"
+})
