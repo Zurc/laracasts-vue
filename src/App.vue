5af5ee90212b5104e950c5dab54902be73720a38
Zurc <cruzjurado@gmail.com>
2017-07-17 11:17:14 +0100

    tabs fully working

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,38 @@
 <template>
   <div id="app">
 
-    <modal v-if="showModal" @close="showModal = false">
-      <h2>Había una vez...</h2>
-      <p>...un puercoespín, un lobo y un zorro</p>
-      <p>El puercoespín era muy tímido</p>
-      <img src="https://thumbs.dreamstime.com/x/cute-cartoon-hedgehog-child-illustration-53892503.jpg" alt="puercoespin" width="50px">
-      <p>El lobo y el zorro querían comerlo</p>
-      <img src="http://2.bp.blogspot.com/_hKotJaLktmE/S76i9N-lFaI/AAAAAAAADfk/PqO9n0NEGJk/s1600/fox_and_wolf_01.jpg" alt="lobo y zorro" width="150px">
-      <p>el puercoespín se cubrió con sus espinas y el lobo y el zorro se pincharon todos</p>
-      <p>y así aprendieron que no tenían que molestar al puercoespín que vivió feliz para siempre</p>
-      <img src="https://thumb9.shutterstock.com/display_pic_with_logo/2822515/542956942/stock-vector-set-of-cute-watercolor-hedgehogs-vector-illustration-542956942.jpg" alt="feliz puercoespin">
-    </modal>
-    <message title="Hello World" body="Lorem ipsum dolor sit amet"></message>
-    <message title="Hello Universe" body="bla bla bla"></message>
+    <tabs>
+      <tab name="Home" selected="true">
+        <h1>Here is the content for Home tab</h1>
+      </tab>
+      <tab name="About Us" >
+        <h1>Here is the content for About Us tab</h1>
+      </tab>
+      <tab name="Culture" >
+        <h1>Here is the content for Culture tab</h1>
+      </tab>
+      <tab name="Vision" >
+        <h1>Here is the content for Vision tab</h1>
+      </tab>
+    </tabs>
 
-    <button class="button is-primary is-outlined" @click="showModal = true">Show modal</Button>
   </div>
 </template>
 
 <script>
-  import message from '@/components/Message'
-  import modal from '@/components/Modal'
+  import tabs from '@/components/Tabs'
+  import tab from '@/components/Tab'
 
   export default {
     name: 'app',
     components: {
-      message, modal
+      tabs, tab
     },
+    props: [
+      'name'
+    ],
     data () {
       return {
-        showModal: false
       }
     }
   }
--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,10 +1,40 @@
 <template>
-  something
+  <div>
+    <div class="tabs is-centered">
+      <ul>
+        <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive}">
+          <a :href="tab.href" @click="selectTab(tab)">{{tab.name}}</a>
+        </li>
+      </ul>
+    </div>
+  
+    <div class="tab-details">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'tabs'
+  name: 'tabs',
+  components: {
+    // tab
+  },
+  data () {
+    return {
+      tabs: []
+    }
+  },
+  methods: {
+    selectTab (selectedTab) {
+      this.tabs.forEach(tab => {
+        tab.isActive = (tab.name === selectedTab.name)
+      })
+    }
+  },
+  created () {
+    this.tabs = this.$children
+  }
 }
 </script>
 
